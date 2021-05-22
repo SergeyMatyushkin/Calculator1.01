b3c89c1e50c5b31e141e77e3a5e77851ddaa7afb
Sergey <gecktor87@gmail.com>
2021-05-22 21:13:29 +0300

    Улучшил макет калькулятора, изображение не получается добавить.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //private
+    private TextView textview;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
